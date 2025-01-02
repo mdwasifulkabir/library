a7c3cff80c6c8c158da74740c29fdbec0cf3a772
wasifkabir <wasifulkabir9016@gmail.com>
2025-01-02 10:24:54 +0800

    Add dialog modal to add a new book

--- script.js
+++ script.js
@@ -16,8 +16,37 @@ addBookToLibrary("Frankenstein", "Mary Shelley", 280, true);
 addBookToLibrary("The Midnight Library", "Matt Haig", 150, true);
 addBookToLibrary("Shogun", "James Clavell", 1152, false);
 
-myLibrary.forEach(book => {
-  const bookCard = document.createElement("div");
-  bookCard.textContent = book.title;
-  document.body.appendChild(bookCard);
+const bookDialog = document.querySelector("#addBook");
+const newBookButton = document.querySelector("#newBook");
+const cancelButton = document.querySelector("#cancelButton");
+const confirmButton = document.querySelector("#confirmButton");
+
+
+newBookButton.addEventListener("click", (event) => {
+  bookDialog.showModal();
+});
+
+confirmButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  bookDialog.close();
+});
+
+myLibrary.forEach(function(book){
+  const cardContainer = document.querySelector(".cardContainer");
+  var bookCard = document.createElement("div");
+
+  var booktitle = document.createElement("div");
+  var bookauthor = document.createElement("div");
+  var bookpages = document.createElement("div");
+
+  booktitle.textContent = "Title: " + book.title;
+  bookauthor.textContent = "Author: " + book.author;
+  bookpages.textContent = "Pages: " + book.pages;
+
+  bookCard.appendChild(booktitle);
+  bookCard.appendChild(bookauthor);
+  bookCard.appendChild(bookpages);
+  bookCard.classList.add("bookCard");
+
+  cardContainer.appendChild(bookCard);
 });
